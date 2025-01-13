b935a5c73ab1d2f33652e7ae996adec91d814cd7
Fulvia Vaz <fulviavaz.dev@gmail.com>
2025-01-13 16:29:10 -0300

    feat: upadte footer

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
       {/* Ajuste para largura fixa e centralização */}
       <div className="w-full max-w-[330px]">
         <div className="bg-slate-400 w-full h-[400px] rounded-b-2xl flex items-end justify-center  bg-[url('/bgimg.jpg')] bg-cover bg-center">
-          <Image src="/Logo kart italia.png" alt="logo" width={90} height={35} className="mb-4"  />
+          <Image src="/Logo kart italia.png" alt="logo" width={180} height={70} className="mb-4"  />
         </div>
         <div className="flex flex-col justify-between my-10">
           <h1 className="text-3xl font-bold">Kart Itália</h1>
@@ -29,13 +29,13 @@ export default function Home() {
             Rod. Dom Pedro I, Km 124 Norte - Valinhos/SP
             Contato: (19) 98446-1515
           </p>
-          <Button>Download</Button>
+          <Button>DOWNLOAD VOUCHER</Button>
         </div>
       </div>
-      <footer className="w-full bg-black">
-        <div className="text-center py-5">
-          <p className="text-white">© 2025 Itau Vouchers</p>
-        </div>
+        <footer className="flex flex-wrap items-center justify-center gap-6 w-full bg-black py-8 2xl:px-20 px-10">
+        <p className="text-white text-sm ">Powered by</p>
+        <Image src="/indico.png" alt="logo itau" width={81} height={25} />
+        <p className="text-white text-[8px] 2xl:text-xs ">CAOS SISTEMAS DINÂMICOS E COMPLEXOS LTDA - Brasil - CNPJ 05.934.733/0001-17</p>
       </footer>
     </main>
   );
